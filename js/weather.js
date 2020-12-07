77eab7f00e0b828e4fb97268c526797e6a07f514
BekoBean <55510721+BekoBean@users.noreply.github.com>
2020-12-07 01:04:37 -0500

    homework 7 final

--- js/weather.js
+++ js/weather.js
@@ -3,11 +3,11 @@ function gettingJSON(){
     document.getElementById("forecast").style.display= "block";
     // Your code here.
 
-    //Set default location if one isn't provided
     var location;
     if(document.querySelector("#location").value!=""){
         location= document.getElementById("location").value;
     }
+    //Set default location if one isn't provided
     else{
         location= "Ann Arbor"
     }
@@ -31,7 +31,7 @@ function gettingJSON(){
 
     
     let key= "72d19f0757f4f609b6f8e727fdaeb22b"; 
-    let query= "api.openweathermap.org/data/2.5/weather?q="+location+"&units="+ format + "&appid=" +key;
+    let query= "https://api.openweathermap.org/data/2.5/weather?q="+location+"&units="+ format + "&appid=" +key;
     // Your code here.  
     
     console.log("Query is :" + query);
@@ -46,10 +46,28 @@ function gettingJSON(){
 
 
     $.getJSON(query,function(json){
+        console.log(JSON.stringify(json));
         //Use returned json to update the values of the three 
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
-
+        loc= json.name;
+        temp= json["main"].temp;
+        console.log(temp);
+        icon = (json["weather"][0].icon);
+        var weatherDescription= json["weather"][0]["description"];
+        var iconLink= "http://openweathermap.org/img/wn/" + icon+ ".png";
+        document.querySelector("#tempImg").setAttribute("src", iconLink);
+        // add temp to forecast and weather description
+        // rn, I only have temperature.
+        //ipdate loation
+        document.querySelector("#loc").innerHTML= loc;
+        document.querySelector("#loc").innerHTML= loc;
+        
+        // add title and alt text to weather image 
+        document.querySelector("#tempImg").alt= weatherDescription;
+        document.querySelector("#tempImg").title= "weather image";
+        console.log("alt is" + document.querySelector("#tempImg").alt);
+        document.querySelector("#temp").innerHTML= (temp + " with " + weatherDescription);
     });
 }
